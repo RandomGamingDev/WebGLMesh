d3ee882040fac6192bf785b58d1bf336eede8605
RandomGamingDev <randomgamingdev@gmail.com>
2025-06-01 17:30:52 -0400

    the actual code stuff

---
+++ examples/basic-example/sketch.js
@@ -0,0 +1,138 @@
+			/**
+			 * Provides requestAnimationFrame in a cross browser way.
+			 * paulirish.com/2011/requestanimationframe-for-smart-animating/
+			 */
+			window.requestAnimationFrame = window.requestAnimationFrame || ( function() {
+				return  window.webkitRequestAnimationFrame ||
+				        window.mozRequestAnimationFrame ||
+				        window.oRequestAnimationFrame ||
+				        window.msRequestAnimationFrame ||
+				        function(  callback, element ) {
+					        window.setTimeout( callback, 1000 / 60 );
+				        };
+			})();
+ 
+			var canvas, 
+			    gl, 
+			    buffer, 
+			    vertex_shader, fragment_shader, 
+			    currentProgram,
+			    vertex_position,
+			    timeLocation,
+			    resolutionLocation,
+			    parameters = {  start_time  : new Date().getTime(), 
+			                    time        : 0, 
+			                    screenWidth : 0, 
+			                    screenHeight: 0 };
+			init();
+			animate();
+			function init() {
+				vertex_shader = document.getElementById('vs').textContent;
+				fragment_shader = document.getElementById('fs').textContent;
+ 
+				canvas = document.querySelector( 'canvas' );
+ 
+				// Initialise WebGL
+ 				try {
+					gl = canvas.getContext( 'webgl2' );
+				} catch(error) {}
+				if (!gl) {
+					throw "cannot create webgl context";
+				}
+ 
+				// Create Vertex buffer (2 triangles)
+				vertex_position = new Mesh.VAO(gl);
+				buffer = new Mesh.VBO(new Float32Array([-1.0, -1.0, 1.0, -1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0]), gl.STATIC_DRAW, gl);
+ 
+				// Create Program
+				currentProgram = createProgram(vertex_shader, fragment_shader);
+
+				timeLocation = gl.getUniformLocation(currentProgram, 'time');
+				resolutionLocation = gl.getUniformLocation(currentProgram, 'resolution');
+			}
+ 
+			function createProgram( vertex, fragment ) {
+				var program = gl.createProgram();
+ 
+				var vs = createShader( vertex, gl.VERTEX_SHADER );
+				var fs = createShader( '#ifdef GL_ES\nprecision highp float;\n#endif\n\n' + fragment, gl.FRAGMENT_SHADER );
+ 
+				if ( vs == null || fs == null ) return null;
+ 
+				gl.attachShader(program, vs);
+				gl.attachShader(program, fs);
+ 
+				gl.deleteShader(vs);
+				gl.deleteShader(fs);
+ 
+				gl.linkProgram(program);
+ 
+				if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+					alert( "ERROR:\n" +
+					"VALIDATE_STATUS: " + gl.getProgramParameter( program, gl.VALIDATE_STATUS ) + "\n" +
+					"ERROR: " + gl.getError() + "\n\n" +
+					"- Vertex Shader -\n" + vertex + "\n\n" +
+					"- Fragment Shader -\n" + fragment );
+ 
+					return null;
+				}
+ 
+				return program;
+ 
+			}
+ 
+			function createShader(src, type) {
+				var shader = gl.createShader(type);
+ 
+				gl.shaderSource(shader, src);
+				gl.compileShader(shader);
+ 
+				if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+					alert((type == gl.VERTEX_SHADER ? "VERTEX" : "FRAGMENT") + " SHADER:\n" + gl.getShaderInfoLog(shader));
+					return null;
+ 
+				}
+				return shader;
+			}
+ 
+			function resizeCanvas( event ) {
+				if (canvas.width != canvas.clientWidth || canvas.height != canvas.clientHeight) {
+					canvas.width = canvas.clientWidth;
+					canvas.height = canvas.clientHeight;
+
+					parameters.screenWidth = canvas.width;
+					parameters.screenHeight = canvas.height;
+
+					gl.viewport( 0, 0, canvas.width, canvas.height );
+				}
+ 
+			}
+ 
+			function animate() {
+				resizeCanvas();
+				render();
+				requestAnimationFrame(animate);
+			}
+ 
+			function render() {
+				if (!currentProgram) return;
+ 
+				parameters.time = new Date().getTime() - parameters.start_time;
+ 
+				gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+ 
+				// Load program into GPU
+				gl.useProgram(currentProgram);
+ 
+				// Set values to program variables
+				gl.uniform1f(timeLocation, parameters.time / 1000);
+				gl.uniform2f(resolutionLocation, parameters.screenWidth, parameters.screenHeight);
+ 
+				// Render geometry
+				buffer.bind();
+				//gl.vertexAttribPointer( vertex_position, 2, gl.FLOAT, false, 0, 0 );
+				vertex_position.link_attrib(vertex_position, 2, gl.FLOAT, false, 0, 0);
+				vertex_position.enable();
+				gl.drawArrays(gl.TRIANGLES, 0, 6);
+				vertex_position.disable();
+			}
---
+++ mesh.js
@@ -0,0 +1,90 @@
+let Mesh = {};
+
+Mesh.VAO = class {
+  constructor(gl) {
+    this.gl = gl;
+    this.id = gl.createVertexArray();
+  }
+  
+  enable() {
+    this.gl.enableVertexAttribArray(this.id);
+  }
+  
+  disable() {
+    this.gl.disableVertexAttribArray(this.id);
+  }
+  
+  link_attrib(index, size, type, normalized, stride, offset) {
+    this.gl.vertexAttribPointer(index, size, type, normalized, stride, offset);
+  }
+  
+  bind() {
+    this.gl.bindVertexArray(this.id);
+  }
+  
+  unbind() {
+    this.gl.bindVertexArray(0);
+  }
+}
+
+Mesh.VBO = class {
+  constructor(vertices, how_use, gl) {
+    this.gl = gl;
+    this.vertices = vertices;
+    this.how_use = how_use;
+    this.id = gl.createBuffer();
+    this.bind();
+    this.data(vertices, how_use);
+  }
+  
+  bind() {
+    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.id);
+  }
+  
+  data(vertices, how_use) {
+    this.gl.bufferData(this.gl.ARRAY_BUFFER, vertices, how_use);
+  }
+  
+  subdata(offset, vertices) {
+    this.gl.bufferSubData(this.gl.ARRAY_BUFFER, offset, vertices);
+  }
+  
+  get_subdata(src_offset, dst, dst_offset, length) {
+    this.gl.getBufferSubData(this.gl.ARRAY_BUFFER, src_offset, dst, dst_offset, length);
+  }
+  
+  delete() {
+    this.gl.deleteBuffer(this.id);
+  }
+}
+
+Mesh.EBO = class {
+  constructor(indices, how_use, gl) {
+    this.gl = gl;
+    this.indices = indices;
+    this.how_use = how_use;
+    this.id = gl.createBuffer();
+    this.bind();
+    this.data(indices, how_use);
+  }
+  
+  bind() {
+    this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, this.id);
+  }
+  
+  data(indices, how_use) {
+    this.gl.bufferData(this.gl.ELEMENT_ARRAY_BUFFER, indices, how_use);
+  }
+  
+  subdata(offset, indices) {
+    this.gl.bufferSubData(this.gl.ELEMENT_ARRAY_BUFFER, offset, indices);
+  }
+  
+  get_subdata(src_offset, dst, dst_offset, length) {
+    this.gl.getBufferSubData(this.gl.ELEMENT_ARRAY_BUFFER, src_offset, dst, dst_offset, length);
+  }
+  
+  delete() {
+    this.gl.deleteBuffer(this.id);
+  }
+}
